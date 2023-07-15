68e35b52bb1868d3d4d49c60a749953a6b350f9a
L. Jiang <l.jiang.1024@gmail.com>
2023-07-15 22:55:01 +0800

    fix: run-in-docker.sh

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -2,10 +2,17 @@
 
 set -e
 
+# touch rclone.conf
+mkdir -p /root/.config/rclone/
+touch    /root/.config/rclone/rclone.conf
+
+# install rclone
 cd "$(mktemp -d)"
 curl -LO https://downloads.rclone.org/v1.63.0/rclone-v1.63.0-linux-amd64.zip
 unzip rclone-v1.63.0-linux-amd64.zip
 cp -v rclone-v1.63.0-linux-amd64/rclone /usr/bin/rclone
 chmod +x /usr/bin/rclone
+
+# process
 cd /src/
 make all

829146c21920678e51969e34ec5c62fc130d5010
L. Jiang <l.jiang.1024@gmail.com>
2023-07-15 22:46:47 +0800

    fix: run-in-docker.sh

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -8,4 +8,4 @@ if [ "${target}" == "" ]; then
   target="all"
 fi
 
-docker run --rm -v "$PWD:/src/" -it python:3.10-bookworm bash -c 'set -e;cd /src/;make '"${target}"''
+docker run --rm -v "$PWD:/src/" -it python:3.10-bookworm bash -c 'set -e;cd /src/;make install;make '"${target}"''

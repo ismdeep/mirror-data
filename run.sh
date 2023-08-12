ed59e02de7937803fc836184fc87270756bae317
L. Jiang <l.jiang.1024@gmail.com>
2023-08-12 10:47:07 +0800

    fix: git commit failed

--- run.sh
+++ run.sh
@@ -9,5 +9,5 @@ git pull
 docker run --rm -v "$PWD:/src/" golang:1.20-bookworm bash /src/run-in-docker.sh "${target}"
 
 git add .
-git commit -am "docs: update"
+git commit -am "docs: update" || true
 git push
